da5cebd474bbc42d86106e780bd10b27d4a75de9
VIKAS KUMAR <154689462+viku01999@users.noreply.github.com>
2025-05-23 18:35:52 +0530

    Add files via upload

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,15 +1,21 @@
 import express from "express";
-import { checkConnectionOfS3BucketByCredentials, upload, uploadFileOnAwsS3 } from "../controllers/fileController";
+import { checkConnectionOfS3BucketByCredentials, getDownloadCompleteFolderSvc, getDownloadNestedCompleteFolderSvc, upload, uploadFileOnAwsS3 } from "../controllers/fileController";
+import { getPresignedFolderUrls, getPresignedFolderUrlsUsignJwtToken } from "../controllers/linkGenerateUrlController";
 
 
 const router = express.Router();
 
 router.post("/uploadFilesOnAWSS3", upload, uploadFileOnAwsS3)
 router.post("/checkConnectionOfS3BucketByCredentials", checkConnectionOfS3BucketByCredentials)
+router.get("/downloadCompleteFolder", getDownloadCompleteFolderSvc)
+router.get("/downloadAllFoldersFile", getDownloadNestedCompleteFolderSvc)
+router.get("/generateDownloadUrls", getPresignedFolderUrls)
+router.get('/generateJwtTokenDownloadUrl', getPresignedFolderUrlsUsignJwtToken)
+
 
 // router.get('/:folder/:fileName',)
 //router.post("/upload file in chunk lasrge file")
- 
+
 
 
 export default router;
